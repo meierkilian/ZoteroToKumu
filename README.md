f2993cfe6a4ce964ef672f06bbb772233cb383bc
meierkilian <47787442+meierkilian@users.noreply.github.com>
2023-10-16 16:45:24 +0100

    Added Theme nodes

--- README.md
+++ README.md
@@ -7,13 +7,10 @@ Open a console and navigate to the folder<br />
 (Optional) If you use virtual environments, create one and activate it.<br />
 Run:  pip (or pip3) install -r requirements.txt<br />
 <br />
-Create file called .env with information from your Zotero account:<br />
+Edit the .env file with information from your Zotero account:<br />
 LIBRARY_TYPE=group    (for accessing a group library)<br />
 or LIBRARY_TYPE=user    (for accessing a user library)<br />
-
-LIBRARY_ID: Your personal library ID is [available here](https://www.zotero.org/settings/keys), in the section "Your userID for use in API calls"<br />
-For group libraries, the ID can be found by opening the group’s page: https://www.zotero.org/groups/groupname, and hovering over the group settings link. The ID is the integer after /groups/<br />
-
+LIBRARY_ID: (in a browser, navigate to the library you want to access and grab its ID from the URL)<br />
 ZOTERO_API_KEY=[create your api key here](https://www.zotero.org/settings/keys)<br />
 Save the .env file<br />
 
@@ -21,10 +18,5 @@ Here's my .env file (for reference)<br />
 ![image](https://github.com/zenskunkworx/ZoteroToKumu/assets/145480414/98cccea0-5570-4cad-bdd2-4c2a5b9ab06b)<br />
 
 Run: python app.py    (or python3 app.py)<br />
-
-Here's a sample View setup in Kumu:<br />
-![image](https://github.com/zenskunkworx/ZoteroToKumu/assets/145480414/abac7ec5-03a3-4be7-9ee5-d0babe0b1f4d)<br />
-
-
 
 
--- app.py
+++ app.py
@@ -9,6 +9,39 @@ library_id: str = os.getenv("LIBRARY_ID")
 library_type: str = os.getenv("LIBRARY_TYPE")
 api_key: str = os.getenv("ZOTERO_API_KEY")
 
+# Describe the structure of information
+themeStructure: dict = {
+    'Docking': [
+        'FixedTarget',
+        'GeneralDocking',
+        { 'MovingTarget': [
+            'GroundVehicle',
+            'OtherMovingTarget',
+            { 'AirbornVehicle': [
+                'FixedWing-FixedWing',
+                'RotaryWing-RotaryWing',
+                'RotaryWing-FixedWing']
+            }
+            ]
+        }
+        ]
+    }
+
+def getSubElems(elem) :
+    subElems: list = []
+    if type(elem) is dict:
+        for key in elem:
+            subElems.append(key)
+            subElems += getSubElems(elem[key])
+    elif type(elem) is list:
+        for e in elem:
+            subElems += getSubElems(e)
+    else:
+        subElems.append(elem)
+    return subElems
+
+allThemes = getSubElems(themeStructure)
+
 zot: zotero.Zotero = zotero.Zotero(library_id=library_id,
                                    library_type=library_type,
                                    api_key=api_key)
@@ -17,36 +50,79 @@ json_data: dict[str,list] = {'elements':[], 'connections':[]}
 item_count: int = 1
 total_items: int = len(items)
 
+
+for theme in allThemes:
+    json_data['elements'].append({'label':theme, 'type':'Theme'})
+
+
+def createSubConnections(elem):
+    names = []
+    if type(elem) is dict:
+        for e in elem:
+            for child in elem[e]:
+                if type(child) is dict:
+                    json_data['connections'].append({'from':e, 'to':list(child.keys())[0], 'type':'InTheme'})
+                    createSubConnections(child)
+                else:
+                    json_data['connections'].append({'from':e, 'to':child, 'type':'InTheme'})
+    
+    return names
+
+createSubConnections(themeStructure)
+
 # Iterate across items in the Zotero Library
 for item in items:
     # Create variables to store attributes of the item
-    label: str = str(item['data']['title'])
-    url: str = str(item['data'].get('url', ""))
-    href: str = item['links']['self'].get('alternate', "")
-    desc: str = str(item['data'].get('abstractNote', ""))
-    item_type: str = str(item['data'].get('itemType', ""))
-    pdate: str = str(item['data'].get('date', ""))
-    publication: str = str(item['data'].get('publicationTitle', ""))
+    try:
+        label: str = str(item['data']['title'])
+        url: str = str(item['data'].get('url', ""))
+        href: str = item['links']['self'].get('alternate', "")
+        desc: str = str(item['data'].get('abstractNote', ""))
+        item_type: str = str(item['data'].get('itemType', ""))
+        pdate: str = str(item['data'].get('date', ""))
+        publication: str = str(item['data'].get('publicationTitle', ""))        
+        all_tags: list = list(item['data'].get('tags', ""))
 
-    # Create a kumu node for the item
-    json_data['elements'].append({'label':label,
-                                  'type':item_type,
-                                  'Zotero Link':href,
-                                  'Original Link':url,
-                                  'Description':desc,
-                                  'Publication':publication,
-                                  'Date':pdate,
-                                  })
 
-    if 'creators' in item['data']:
-        # Add authors of the item as Elements and connect them to the item with an Edge
-        for creator in item['data']['creators']:
-            author_name = creator.get('firstName',"---") + " " + creator.get('lastName',"---")
-            json_data['elements'].append({'label':author_name, 'type':'Person'})
-            json_data['connections'].append({'from':author_name, 'to':label, 'type':'Author'})
+        all_tags_dict = []
+        for t in all_tags:
+            all_tags_dict.append(dict(t))
 
-    print(str(item_count) + "/" + str(total_items) + " - ", item['data']['title'])
-    item_count += 1
+        relevant_tags: list = []
+        for t in all_tags_dict:
+            if 'type' not in t:
+                relevant_tags.append(t['tag'])
+
+
+
+        # Create a kumu node for the item
+        json_data['elements'].append({'label':label,
+                                      'type':item_type,
+                                      'Zotero Link':href,
+                                      'Original Link':url,
+                                      'Description':desc,
+                                      'Publication':publication,
+                                      'Date':pdate,
+                                      'Tag':relevant_tags
+                                      })
+
+        if 'creators' in item['data']:
+            # Add authors of the item as Elements and connect them to the item with an Edge
+            for creator in item['data']['creators']:
+                author_name = creator.get('firstName',"---") + " " + creator.get('lastName',"---")
+                json_data['elements'].append({'label':author_name, 'type':'Person'})
+                json_data['connections'].append({'from':author_name, 'to':label, 'type':'Authorship'})
+
+        for tag in relevant_tags:
+            if tag in allThemes:
+                json_data['connections'].append({'from':tag, 'to':label, 'type':'InTheme'})            
+
+        print(str(item_count) + "/" + str(total_items) + " - ", item['data']['title'])
+        item_count += 1
+        
+    except Exception as e:
+        print("---------------FISHY ITEM---------------")
+        print(item)
 
 # Write the JSON data to a file
 with open('zotero.json', 'w',encoding='utf-8') as outfile:
